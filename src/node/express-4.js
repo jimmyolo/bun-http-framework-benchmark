a841ecadf4ad6554b68820f840aa2e766372cd41
jimmyolo <victor0801x@gmail.com>
2025-04-28 22:59:47 +0800

    add express 4 & 5

---
+++ src/node/express-4.js
@@ -0,0 +1,15 @@
+const express = require('express-4')
+
+express()
+  .get('/', (req, res) => {
+    res.setHeader('content-type', 'text/plain').send('Hi')
+  })
+  .post('/json', express.json(), ({ body }, res) => {
+    res.json(body)
+  })
+  .get('/id/:id', ({ params: { id }, query: { name } }, res) => {
+    res.setHeader('x-powered-by', 'benchmark')
+      .setHeader('content-type', 'text/plain')
+      .send(`${id} ${name}`)
+  })
+  .listen(3000)
